b5ed6b0e418128796da7f99ca8a47bc765dd650b
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-05 12:38:06 +1000

    Added current user signed in info to root page after login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def profiles
   end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_profile
+      @profile = Profile.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def profile_params
+      params.require(:profile).permit(:avatar, :description, :name, :gender)
+    end
+    
 end
